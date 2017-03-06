44a54e0d2620f49cfce5c040a54032ca78b6ec2b
Prasanna Kumar Patra <prasannap@tataclassedge.com>
2017-03-06 09:12:27 +0530

    clusters

---
+++ clusterlist.js
@@ -0,0 +1,14 @@
+var clusters = ['ADRIATICA',
+    'AMAZONA', 'ANTARCTICA',
+    'AQUA', 'ARABIANA',
+    'ARCTICA', 'ATLANTICA',
+    'Beverelee', 'CASPIANA', 'COLUMBIA',
+    'Dionna', 'Erwina', 'Exotica', 'GARDENIA',
+    'GENEVIA', 'Genevieve', 'GIARDINO', 'Irvetta',
+    'MAGDELENA', 'MARINA', 'MEDITERRANEA', 'Morgana',
+    'Muriel', 'NAUTICA', 'NYASIA', 'OCEANIA', 'PACIFICA',
+    'PERSIANA', 'Rhine', 'Rilletta', 'RIVER DALE',
+    'RIVER RETREAT', 'RIVER VIEW',
+    'RIVERSCAPE', 'VICTORIA',
+    'VIVA', 'Viviana', 'VOLGA'
+]
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -465,7 +465,23 @@ angular.module('starter.controllers', [])
             $('.container').stop().removeClass('active');
         });
 
+        $scope.wings = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
         $scope.sizes = [38, 40, 42, 44]
+        $scope.clusters = ['ADRIATICA',
+            'AMAZONA', 'ANTARCTICA',
+            'AQUA', 'ARABIANA',
+            'ARCTICA', 'ATLANTICA',
+            'Beverelee', 'CASPIANA', 'COLUMBIA',
+            'Dionna', 'Erwina', 'Exotica', 'GARDENIA',
+            'GENEVIA', 'Genevieve', 'GIARDINO', 'Irvetta',
+            'MAGDELENA', 'MARINA', 'MEDITERRANEA', 'Morgana',
+            'Muriel', 'NAUTICA', 'NYASIA', 'OCEANIA', 'PACIFICA',
+            'PERSIANA', 'Rhine', 'Rilletta', 'RIVER DALE',
+            'RIVER RETREAT', 'RIVER VIEW',
+            'RIVERSCAPE', 'VICTORIA',
+            'VIVA', 'Viviana', 'VOLGA'
+        ]
+
 
         // Set Header
         $scope.$parent.clearFabs();
